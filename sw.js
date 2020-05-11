1ee4397a99f170e61f11f584067e6625210ff6d2
sukasukangoding <sukasukangoding@gmail.com>
2020-05-11 20:56:55 +0700

    init

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("push", function (event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = "Push message no payload";
+  }
+  var options = {
+    body: body,
+    icon: "img/notification.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1,
+    },
+  };
+  event.waitUntil(
+    self.registration.showNotification("Push Notification", options)
+  );
+});
